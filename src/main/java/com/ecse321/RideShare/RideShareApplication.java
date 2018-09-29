ea24957ed18e19b6edb8a329dc6d0c058ea88b4d
norinmura <nswe.nrin@gmail.com>
2018-09-29 15:55:45 -0400

    testing database

---
+++ src/main/java/com/ecse321/RideShare/RideShareApplication.java
@@ -0,0 +1,50 @@
+package com.ecse321.RideShare;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+
+public class RideShareApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(RideShareApplication.class);
+  
+    public static void main(String[] args) {
+		SpringApplication.run(RideShareApplication.class, args);
+	}
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    
+    @RequestMapping(path="/")
+    public String greeting() {
+        return "Hello, world! ";
+    }
+    
+    @RequestMapping(path="/users", method=RequestMethod.GET)
+    public String index() {
+        List<Map<String,Object>> list;
+        list = jdbcTemplate.queryForList("select * from user_table");
+        return list.toString();
+    }
+
+    @RequestMapping(path="/users/{id}", method=RequestMethod.GET)
+    public String read(@PathVariable String id) {
+        List<Map<String,Object>> list;
+        list = jdbcTemplate.queryForList("select * from user_table where userid = ?", id);
+        return list.toString();
+    }
+}
+
---
+++ src/main/java/com/ecse321/RideShare/TestController.java
@@ -0,0 +1,14 @@
+package com.ecse321.RideShare;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
